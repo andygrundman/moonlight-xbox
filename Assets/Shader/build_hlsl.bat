052f394581baab189865d4731920cc4c09b994b0
Andy Grundman <andy@hybridized.org>
2025-11-25 18:01:44 -0500

    Xbox One only supports shader model 4, revert 5.0 binaries

--- Assets/Shader/build_hlsl.bat
+++ Assets/Shader/build_hlsl.bat
@@ -1,3 +1,3 @@
-fxc /T vs_5_0 /Fo d3d11_vertex.fxc d3d11_vertex.hlsl
+fxc /T vs_4_0_level_9_3 /Fo d3d11_vertex.fxc d3d11_vertex.hlsl
 
-fxc /T ps_5_0 /Fo d3d11_yuv420_pixel.fxc d3d11_yuv420_pixel.hlsl
+fxc /T ps_4_0_level_9_3 /Fo d3d11_yuv420_pixel.fxc d3d11_yuv420_pixel.hlsl
